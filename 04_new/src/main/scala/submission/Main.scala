3028e79dba2b2224c79fec86cc9b37852a5360b3
norang <noranglemon@gmail.com>
2016-10-29 13:06:35 +0900

    작업 옮기기

--- 04_new/src/main/scala/submission/Main.scala
+++ 04_new/src/main/scala/submission/Main.scala
@@ -12,16 +12,16 @@ object Subtype_new {
     type G
     type H
 
-    type Func1 = { val a: A } => { val b: B }
-    type Func2 = { val b: B } => { val a: A }
+    type Func1 = {val a: A} => {val b: B}
+    type Func2 = {val b: B} => {val a: A}
 
     type Ty1 = {
-      def apply: { val func: Func1 ; val c: C } => { val b: B ; val d: D }
+      def apply: {val func: Func1; val c: C} => {val b: B; val d: D}
       val g: G
     }
 
     type Ty2 = {
-      def apply: { val func: Func2 ; val e: E } => { val b: B ; val f: F }
+      def apply: {val func: Func2; val e: E} => {val b: B; val f: F}
       val h: H
     }
 
@@ -29,14 +29,19 @@ object Subtype_new {
      Find suitable common supertype of Ty1 and Ty2,
      and replace "Any" with that type.
      */
-    type CommonTy = Any
+    type CommonTy = {
+      def apply: {val func: {val a: A; val b: B} => AnyRef{}; val c: C; val e: E} => {val b: B}
+    }
 
     /*
      Fill in the apply function here.
      It should *USE* x.
      As a consequence, the output of this function might change with different x given.
      */
-    def apply(x: CommonTy, _a: A, _b: B, _c: C, _d: D, _e: E, _f: F, _g: G, _h: H) =
-      ???
+    def apply(x: CommonTy, _a: A, _b: B, _c: C, _d: D, _e: E, _f: F, _g: G, _h: H) = {
+      val _func: {val a: A; val b: B} => AnyRef{} =
+        x => new AnyRef{}
+      x.apply(new {val func = _func; val c = _c; val e = _e})
+    }
   }
 }
