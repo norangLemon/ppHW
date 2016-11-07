905a852c5b3fb08b4fca5048303cef9af95c16d3
norang <noranglemon@gmail.com>
2016-11-07 16:35:09 +0900

    6번 숙제 완료

--- 06/src/main/scala/submission/Main.scala
+++ 06/src/main/scala/submission/Main.scala
@@ -23,18 +23,52 @@ object MoreAbstractClass {
   class BiIterableList[A](val data: List[A]) extends BiIterable[A] {
     def biIter: BiIter[A] = new MyList[A](data)(0)
   }
-/*
+
+  class MyNode[A](tree: BiIterableTree[A])(i: Int, len: Int) extends BiIter[A] {
+    val idx = i
+    def getNext: MyNode[A] = if (i == len) this else new MyNode[A](tree)(i + 1, len)
+    def getPrev: MyNode[A] = if (i == 0) this else new MyNode[A](tree)(i - 1, len)
+
+    def getValue: Option[A] = {
+      def at(tree:BiIterableTree[A], i:Int): (A, Int) =
+        tree match {
+          case Node(value, _ , _) if(i == 0) => (value, i)
+          case Node(value, Empty(), Empty()) => (value, i)
+          case Node(value, Empty(), right) => at(right, i-1)
+          case Node(value, left, Empty()) => at(left, i-1)
+          case Node(value, left, right) => {
+            val end = at(left, i-1)
+            if (end._2 == 0) end
+            else at(right, end._2 - 1)
+          }
+        }
+      if (i == len) None
+      else Some(at(tree, i)._1)
+    }
+  }
+
+  class MyEmpty[A]() extends BiIter[A] {
+    def getValue: Option[A] = None
+    def getNext: MyEmpty[A] = this
+    def getPrev: MyEmpty[A] = this
+  }
+
   sealed abstract class BiIterableTree[A] extends BiIterable[A] {
     //You are allowed to write something here
+    def getNumOfNode: Int = {
+      this match {
+        case Empty() => 0
+        case Node(value, left, right) => 1 + left.getNumOfNode + right.getNumOfNode
+      }
+    }
   }
 
   case class Empty[A]() extends BiIterableTree[A] {
-    def biIter = ???
+    def biIter = new MyEmpty[A]
   }
 
   case class Node[A](value: A, left: BiIterableTree[A], right: BiIterableTree[A])
       extends BiIterableTree[A] {
-    def biIter = ???
+    def biIter = new MyNode[A](this)(0, this.getNumOfNode)
   }
-  */
 }
