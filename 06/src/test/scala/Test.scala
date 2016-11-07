d85a0631197a4ef43f5d48e6b83341e7090f513c
norang <noranglemon@gmail.com>
2016-11-07 16:34:48 +0900

    06번 테스트 케이스 추가

--- 06/src/test/scala/Test.scala
+++ 06/src/test/scala/Test.scala
@@ -20,7 +20,6 @@ class TestSuite extends FunSuite {
     assert(by.biIter.getPrev.getValue == None)
     assert(by.biIter.getNext.getValue == None)
   }
-/*
   test("BiIterableTree") {
     val x = Node[String]("A", Node("B", Empty(), Empty()), Node("C", Empty(), Empty()))
     assert(x.biIter.getValue == Some("A"))
@@ -31,5 +30,18 @@ class TestSuite extends FunSuite {
     assert(x.biIter.getNext.getNext.getNext.getValue == None)
     assert(x.biIter.getNext.getNext.getNext.getPrev.getValue == Some("C"))
   }
-  */
+    test("my - BiIterableTree") {
+    val x = Node[String]("1", Node("2", Empty(), Node("3", Empty(), Empty())), Node("4", Node("5", Empty(), Empty()), Empty()))
+    assert(x.biIter.getValue == Some("1"))
+    assert(x.biIter.getPrev.getValue == Some("1"))
+    assert(x.biIter.getPrev.getNext.getValue == Some("2"))
+    assert(x.biIter.getNext.getValue == Some("2"))
+    assert(x.biIter.getNext.getNext.getValue == Some("3"))
+    assert(x.biIter.getNext.getNext.getNext.getValue == Some("4"))
+    assert(x.biIter.getNext.getNext.getNext.getNext.getValue == Some("5"))
+    assert(x.biIter.getNext.getNext.getNext.getNext.getNext.getValue == None)
+    assert(x.biIter.getNext.getNext.getNext.getNext.getNext.getNext.getValue == None)
+    assert(x.biIter.getNext.getNext.getNext.getNext.getNext.getNext.getPrev.getValue == Some("5"))
+    assert(x.biIter.getNext.getNext.getNext.getPrev.getValue == Some("3"))
+  }
 }
