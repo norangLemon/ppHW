983f0e2fc18268390b7fdbcebe810fa49d0bb64d
norang <noranglemon@gmail.com>
2016-11-30 17:21:00 +0900

    complete hw 07

--- 07/submission/Main.scala
+++ 07/submission/Main.scala
@@ -10,11 +10,22 @@ object TraitMixin extends App {
    If you use normal "Double" operators, you may not need to worry about it at all.
    */
   trait ComplexNumberAdd extends ComplexNumberSpec {
-    def add(that: ComplexNumberSpec): ComplexNumberSpec = ???
+    def add(that: ComplexNumberSpec): ComplexNumberSpec = new ComplexNumberImpl (
+      true,
+      this.real + that.real,
+      this.imaginary + that.imaginary
+    )
   }
 
   trait ComplexNumberMult extends ComplexNumberSpec {
-    def mult(that: ComplexNumberSpec): ComplexNumberSpec = ???
+    def mult(that: ComplexNumberSpec): ComplexNumberSpec =  new ComplexNumberImpl (
+        false,
+        this.magnitude * that.magnitude,
+        {
+          import ComplexNumberSpec.normalizeAngle;
+          normalizeAngle(this.angle + that.angle)
+        }
+      )
   }
 
   /*
@@ -31,7 +42,18 @@ object TraitMixin extends App {
       extends ComplexNumberSpec
       with ComplexNumberAdd
       with ComplexNumberMult {
-    def eval(coeffs: List[ComplexNumberEval]): ComplexNumberSpec = ???
+    def eval(coeffs: List[ComplexNumberEval]): ComplexNumberSpec = {
+      def calc(coeffs: List[ComplexNumberEval], pow: ComplexNumberImpl, sum: ComplexNumberImpl):ComplexNumberSpec =
+      coeffs match{
+        case Nil => sum
+        case a :: list => {
+          val p = pow.mult(this);
+          val s = sum.add(pow.mult(a))
+          calc(list, new ComplexNumberImpl(true, p.real, p.imaginary), new ComplexNumberImpl(true, s.real, s.imaginary))
+        }
+      }
+      calc(coeffs, new ComplexNumberImpl(true, 1, 0), new ComplexNumberImpl(true, 0, 0))
+    }
   }
 
   /*
@@ -48,6 +70,22 @@ object TraitMixin extends App {
       with ComplexNumberAdd
       with ComplexNumberMult
       with ComplexNumberEval {
+
+    import ComplexNumberSpec._
+    def makeRectangular(real: Double, imaginary: Double): ComplexNumberSpec = {
+      new ComplexNumberImpl(true, real, imaginary)
+    }
+
+    def makePolar(magnitude: Double, angle: Double): ComplexNumberSpec = {
+      new ComplexNumberImpl(false, magnitude, angle)
+    }
+
+    val (a, b) = rectangularToPolar(arg1, arg2);
+    val (c, d) = polarToRectangular(arg1, arg2);
+    override val real: Double = if(isRectangular) arg1 else c;
+    override val imaginary: Double = if(isRectangular) arg2 else d;
+    override val magnitude: Double = if (!isRectangular) arg1 else a;
+    override val angle: Double = if (!isRectangular) arg2 else b;
   }
 
 
